aef13b20e0b1c21612b86ba3f0c7eb1e43ced2b0
sombrero104 <sombrero.khy@gmail.com>
2021-12-07 20:34:12 +0900

    .

--- src/test/java/me/springadvanced01/trace/strategy/ContextV1Test.java
+++ src/test/java/me/springadvanced01/trace/strategy/ContextV1Test.java
@@ -96,10 +96,26 @@ public class ContextV1Test {
      */
     @Test
     void strategyV4() {
-        ContextV1 context1 = new ContextV1(() -> log.info("비즈니스 로직 1 실행"));
-        context1.execute();
-        ContextV1 context2 = new ContextV1(() -> log.info("비즈니스 로직 2 실행"));
-        context2.execute();
+        /**
+         * 선 조립, 후 실행
+         *
+         * 여기서 이야기하고 싶은 부분은 Context의 내부 필드에 Strategy를 두고 사용하는 부분이다.
+         * 이 방식은 Context와 Strategy를 실행 전에 원하는 모양으로 조립해두고,
+         * 그 다음에 Context를 실행하는 선 조립, 후 실행 방식에서 매우 유용하다.
+         * Context와 Strategy를 한번 조립하고 나면 이후로는 Context를 실행하기만 하면 된다.
+         * 우리가 스프링으로 애플리케이션을 개발할 때 애플리케이션 로딩 시점에 의존관계 주입을 통해 필요한 의존관계를 모두 맺어두고 난 다음에
+         * 실제 요청을 처리하는 것과 같은 원리이다.
+         *
+         * 이 방식의 단점은 Context와 Strategy를 조립한 이후에는 전략을 변경하기가 번거롭다는 점이다.
+         * 물론 Context에 setter를 제공해서 Strategy를 넘겨 받아 변경하면 되지만,
+         * Context를 싱글톤으로 사용할 때는 동시성 이슈 등 고려할 점이 많다.
+         * 그래서 전략을 실시간으로 변경해야 하면 차라리
+         * Context를 하나 더 생성하고 그곳에 다른 Strategy를 주입하는 것이 더 나은 선택일 수 있다.
+         */
+        ContextV1 context1 = new ContextV1(() -> log.info("비즈니스 로직 1 실행")); // 선 조립,
+        context1.execute(); // 후 실행
+        ContextV1 context2 = new ContextV1(() -> log.info("비즈니스 로직 2 실행")); // 선 조립,
+        context2.execute(); // 후 실행
     }
 
 }
