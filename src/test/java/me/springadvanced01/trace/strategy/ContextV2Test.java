e289444ef273aa1a0b5a822d6965238af1131d86
sombrero104 <sombrero.khy@gmail.com>
2021-12-07 21:01:52 +0900

    .

---
+++ src/test/java/me/springadvanced01/trace/strategy/ContextV2Test.java
@@ -0,0 +1,29 @@
+package me.springadvanced01.trace.strategy;
+
+import lombok.extern.slf4j.Slf4j;
+import me.springadvanced01.trace.strategy.code.strategy.ContextV2;
+import me.springadvanced01.trace.strategy.code.strategy.StrategyLogic1;
+import me.springadvanced01.trace.strategy.code.strategy.StrategyLogic2;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV2Test {
+
+    /**
+     * 전략 패턴 적용
+     *
+     * Context와 Strategy를 '선 조립 후 실행'하는 방식이 아니라
+     * Context를 실행할 때마다 전략을 인수로 전달한다.
+     * 클라이언트는 Context를 실행하는 시점에 원하는 Strategy를 전달할 수 있다.
+     * 따라서 이전 방식과 비교해서 원하는 전략을 더욱 유연하게 변경할 수 있다.
+     * 테스트 코드를 보면 하나의 Context만 생성한다.
+     * 그리고 하나의 Context에 실행 시점에 여러 전략을 인수로 전달해서 유연하게 실행한다.
+     */
+    @Test
+    void strategyV1() {
+        ContextV2 context = new ContextV2();
+        context.execute(new StrategyLogic1());
+        context.execute(new StrategyLogic2());
+    }
+
+}
