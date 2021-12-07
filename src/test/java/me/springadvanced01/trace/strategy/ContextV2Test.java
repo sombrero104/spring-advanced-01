3efff676ea7954219ec0409f61b767214fea9478
sombrero104 <sombrero.khy@gmail.com>
2021-12-07 21:10:23 +0900

    .

--- src/test/java/me/springadvanced01/trace/strategy/ContextV2Test.java
+++ src/test/java/me/springadvanced01/trace/strategy/ContextV2Test.java
@@ -2,6 +2,7 @@ package me.springadvanced01.trace.strategy;
 
 import lombok.extern.slf4j.Slf4j;
 import me.springadvanced01.trace.strategy.code.strategy.ContextV2;
+import me.springadvanced01.trace.strategy.code.strategy.Strategy;
 import me.springadvanced01.trace.strategy.code.strategy.StrategyLogic1;
 import me.springadvanced01.trace.strategy.code.strategy.StrategyLogic2;
 import org.junit.jupiter.api.Test;
@@ -24,6 +25,36 @@ public class ContextV2Test {
         ContextV2 context = new ContextV2();
         context.execute(new StrategyLogic1());
         context.execute(new StrategyLogic2());
+    }
+
+    /**
+     * 전략 패턴 익명 내부 클래스
+     */
+    @Test
+    void strategyV2() {
+        ContextV2 context = new ContextV2();
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 1 실행");
+            }
+        });
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 2 실행");
+            }
+        });
+    }
+
+    /**
+     * 전략 패턴 익명 내부 클래스2, 람다
+     */
+    @Test
+    void strategyV3() {
+        ContextV2 context = new ContextV2();
+        context.execute(() -> log.info("비즈니스 로직 1 실행"));
+        context.execute(() -> log.info("비즈니스 로직 2 실행"));
     }
 
 }
