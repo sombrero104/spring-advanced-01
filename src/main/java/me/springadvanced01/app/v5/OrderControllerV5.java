25de3597c5d92d4d1f4f2e9d16b9d01a0a20b363
sombrero104 <sombrero.khy@gmail.com>
2021-12-14 20:32:33 +0900

    .

--- src/main/java/me/springadvanced01/app/v5/OrderControllerV5.java
+++ src/main/java/me/springadvanced01/app/v5/OrderControllerV5.java
@@ -18,6 +18,8 @@ public class OrderControllerV5 {
         /**
          * OrderControllerV5가 싱글톤 빈이기 때문에 위 template 객체도 하나이다.
          * 위처럼 생성하거나 스프링 빈으로 template을 등록해서 사용해도 된다.
+         * 이 방식의 장점은 테스트할 때 TraceTemplate을 크게 신경쓰지 않고,
+         * LogTrace만 Mock으로 만들거나해서 전달해 주면 간단하게 테스트할 수 있다는 장점이 있다.
          */
     }
 
